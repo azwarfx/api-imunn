9a83ed0a3cc551df36f94dff34fa0d6492b37009
azwarazuhri <data.azwarazuhri@gmail.com>
2019-07-08 00:52:38 +0700

    bug fixing login

--- imunn-login.php
+++ imunn-login.php
@@ -1,24 +1,26 @@
 <?php
-require_once('koneksi.php');
 if($_SERVER['REQUEST_METHOD']=='POST'){
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $sql = "SELECT FROM users WHERE (email='$username' AND password='$password') OR (phonenumber='$username' AND password='$password')";
+    require_once('koneksi.php');
+    $sql = "SELECT * FROM users WHERE (email='$username' AND password='$password') 
+    OR (phonenumber='$username' AND password='$password')";
     $r =mysqli_query($con,$sql);
     $result = array();
     $row = mysqli_fetch_array($r);
-    if($row['email']||$row['password']!=null){
-        array_push($result,array(
-            "status"=>"failed",
-            "notes"=>"User tidak di temukan"
-        ));
-        echo json_encode(array('result'=>$result));
-    }else{
+    if($row['email']!=null || $row['phonenumber']!=null){
         array_push($result,array(
             "status"=>"succes",
             "fullname"=>$row['fullname'],
             "email"=>$row['email'],
             "phonenumber"=>$row['phonenumber']
+        ));
+        echo json_encode(array('result'=>$result));
+
+    }else{
+        array_push($result,array(
+            "status"=>"failed",
+            "notes"=>"User tidak di temukan"
         ));
         echo json_encode(array('result'=>$result));
     }
